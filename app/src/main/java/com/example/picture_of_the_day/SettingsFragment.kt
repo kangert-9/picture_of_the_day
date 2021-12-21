2544dfcba4ab399ba7b0b0856d8df843b248ef53
kange <kangerts9@gmail.com>
2021-12-21 20:38:49 +0300

    less_2

---
+++ app/src/main/java/com/example/picture_of_the_day/SettingsFragment.kt
@@ -0,0 +1,12 @@
+package com.example.picture_of_the_day
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class SettingsFragment : Fragment() {
+
+}
