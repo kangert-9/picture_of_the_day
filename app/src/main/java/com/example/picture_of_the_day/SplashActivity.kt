206bbbddaddd59b66bcca097a535d1b90c4ae18a
kange <kangerts9@gmail.com>
2022-01-21 19:17:38 +0300

    lesson_8

---
+++ app/src/main/java/com/example/picture_of_the_day/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.picture_of_the_day
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    var handler = Handler()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        handler.postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+    override fun onDestroy() {
+        handler.removeCallbacksAndMessages(null)
+        super.onDestroy()
+    }
+}
